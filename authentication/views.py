dee97b850304c49ad787bf0c7d789e1eeda7c512
Aryan Chaubal <f20190130@hyderabad.bits-pilani.ac.in>
2022-04-24 13:13:30 +0530

    Fix: google login issues

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,9 @@
+from .googleviews import GoogleOAuth2AdapterIdToken
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 
 
 class GoogleLogin(SocialLoginView):
-    adapter_class = GoogleOAuth2Adapter
-    callback_url = 'CALLBACK_URL_YOU_SET_ON_GOOGLE'
+    adapter_class = GoogleOAuth2AdapterIdToken
     client_class = OAuth2Client
